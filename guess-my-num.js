662921e6a369d2ca996f3d14f33aa3bb080e5737
eric shin <ericshin7824@gmail.com>
2022-04-09 23:19:22 -0700

    Guess-my-number first

---
+++ guess-my-num.js
@@ -0,0 +1,72 @@
+"use strict";
+
+const guessMyNumber = document.querySelector("#guess-my-number");
+const screenScore = document.querySelector("#score");
+const screenNumber = document.querySelector("#number");
+const screenHighScore = document.querySelector("#highscore");
+const screenGuess = document.querySelector("#guess");
+const screenMessage = document.querySelector("#message");
+
+let secretNumber = Math.trunc(Math.random() * 10) + 1;
+let score = 10;
+let highScore = 0;
+
+const displayMessage = function (message) {
+    screenMessage.textContent = message;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+    const guess = Number(screenGuess.value);
+
+    if (guess === secretNumber) {
+        // when player win.
+        displayMessage("Currect Number!");
+        screenNumber.textContent = secretNumber;
+        guessMyNumber.classList.add("winner");
+
+        if (score > highScore) {
+            highScore = score;
+            screenHighScore.textContent = highScore;
+        }
+
+        // when player didnt put a number.
+    } else if (!guess) {
+        displayMessage("No Number!");
+        guessMyNumber.classList.add("invalid");
+
+        // when guess is too high or low.
+    } else if (guess !== secretNumber) {
+        if (score > 1) {
+            displayMessage(guess > secretNumber ? "The number is too high!" : "The number is too low!");
+
+            guessMyNumber.classList.add("invalid");
+
+            score = score - 1;
+            screenScore.textContent = score;
+        } else {
+            displayMessage("You lost the game!");
+            guessMyNumber.classList.add("invalid");
+
+            screenScore.textContent = 0;
+        }
+    }
+    // remove class after delay
+    setTimeout(() => {
+        guessMyNumber.classList.remove("invalid");
+    }, 300);
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+    score = 10;
+    secretNumber = Math.trunc(Math.random() * 10) + 1;
+
+    displayMessage("Start guessing...");
+
+    guessMyNumber.classList.remove("winner");
+
+    screenScore.textContent = score;
+
+    screenGuess.value = "";
+
+    screenNumber.textContent = "?";
+});
