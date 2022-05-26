beb281dd35d953afc008d7a7ca346555389d5cbe
eric shin <ericshin7824@gmail.com>
2022-05-25 17:03:51 -0700

    devided from my portfolio web

--- guess-my-num.js
+++ guess-my-num.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
-const guessMyNumber = document.querySelector("#guess-my-number");
-const screenScore = document.querySelector("#score");
-const screenNumber = document.querySelector("#number");
-const screenHighScore = document.querySelector("#highscore");
-const screenGuess = document.querySelector("#guess");
-const screenMessage = document.querySelector("#message");
+const guessMyNumber = document.querySelector('#guess-my-number');
+const screenScore = document.querySelector('#score');
+const screenNumber = document.querySelector('#number');
+const screenHighScore = document.querySelector('#highscore');
+const screenGuess = document.querySelector('#guess');
+const screenMessage = document.querySelector('#message');
 
 let playing, secretNumber, score, highScore;
 
@@ -22,28 +22,28 @@ function init() {
 
     displayMessage(`<p class="rules"><span>Find The Secret Number</span><span>(Between 1 and 10)</span></p>`);
 
-    guessMyNumber.classList.remove("winner");
-    guessMyNumber.classList.remove("loser");
-    guessMyNumber.classList.remove("active");
+    guessMyNumber.classList.remove('winner');
+    guessMyNumber.classList.remove('loser');
+    guessMyNumber.classList.remove('active');
 
     screenScore.textContent = score;
 
-    screenGuess.value = "";
+    screenGuess.value = '';
 
     screenNumber.innerHTML = `<p class="press-start"><span>Press START</span><span>to PLAY.</span></p>`;
 }
 init();
 
 // check button
-document.querySelector(".check").addEventListener("click", function () {
+document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(screenGuess.value);
     // check playing
     if (playing) {
         if (guess === secretNumber) {
             // when player win.
-            displayMessage("Currect Number!");
+            displayMessage('Currect Number!');
             screenNumber.textContent = secretNumber;
-            guessMyNumber.classList.add("winner");
+            guessMyNumber.classList.add('winner');
 
             if (score > highScore) {
                 highScore = score;
@@ -52,24 +52,24 @@ document.querySelector(".check").addEventListener("click", function () {
 
             // when player didnt put a number.
         } else if (!guess) {
-            displayMessage("No Number!");
-            guessMyNumber.classList.add("invalid");
+            displayMessage('No Number!');
+            guessMyNumber.classList.add('invalid');
 
             // when guess is too high or low.
         } else if (guess !== secretNumber) {
             if (score > 1) {
-                displayMessage(guess > secretNumber ? "The number is too high!" : "The number is too low!");
+                displayMessage(guess > secretNumber ? 'The number is too high!' : 'The number is too low!');
 
-                guessMyNumber.classList.add("invalid");
+                guessMyNumber.classList.add('invalid');
 
                 score = score - 1;
                 screenScore.textContent = score;
 
                 // when score is 0
             } else {
-                displayMessage("You lost the game!");
-                guessMyNumber.classList.add("invalid");
-                guessMyNumber.classList.add("loser");
+                displayMessage('You lost the game!');
+                guessMyNumber.classList.add('invalid');
+                guessMyNumber.classList.add('loser');
                 screenScore.textContent = 0;
             }
         }
@@ -77,12 +77,12 @@ document.querySelector(".check").addEventListener("click", function () {
 
     // remove class after delay
     setTimeout(() => {
-        guessMyNumber.classList.remove("invalid");
+        guessMyNumber.classList.remove('invalid');
     }, 300);
 });
 
 // to reset
-document.querySelector(".reset").addEventListener("click", function () {
+document.querySelector('.reset').addEventListener('click', function () {
     // set bact to default
     if (playing) {
         init();
@@ -90,13 +90,13 @@ document.querySelector(".reset").addEventListener("click", function () {
 });
 
 // to start
-document.querySelector(".start").addEventListener("click", () => {
+document.querySelector('.start').addEventListener('click', () => {
     // check player playing or not
     if (!playing) {
         // turn on playing
         playing = true;
-        screenNumber.textContent = "?";
-        displayMessage("Start guessing..");
-        guessMyNumber.classList.add("active");
+        screenNumber.textContent = '?';
+        displayMessage('Start guessing..');
+        guessMyNumber.classList.add('active');
     }
 });
